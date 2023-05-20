7b56f9a2ace13b968f0b969511c50114516cafbe
Nellie McKesson <nelliemckesson@gmail.com>
2023-05-20 15:57:27 -0700

    mobile styles

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -17,3 +17,19 @@ dropdownHeaders.forEach(function(header) {
     }
   });
 });
+
+const mobileNavDisplay = document.querySelector(".mobile-nav-toggle");
+
+mobileNavDisplay.addEventListener("click", function() {
+	const menuBody = this.nextElementSibling;
+
+	if (menuBody.classList.contains("active")) {
+	  menuBody.classList.remove("active");
+	  menuBody.setAttribute("aria-hidden", "true");
+	  this.setAttribute("aria-expanded", "false");
+	} else {
+	  menuBody.classList.add("active");
+	  menuBody.setAttribute("aria-hidden", "false");
+	  this.setAttribute("aria-expanded", "true");
+	}
+});
